0fe3751de00bb1273670b926c2dd031b766c4025
kodyds <kodyglock@gmail.com>
2022-06-18 20:41:05 +0800

    version 1

--- ui/js/ide.js
+++ ui/js/ide.js
@@ -1,36 +1,37 @@
 let editor;
-
-window.onload = function() {
-    editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/c_cpp");
-}
+window.onload = function () {
+  editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.session.setMode("ace/mode/c_cpp");
+  changeLanguage();
+};
 
 function changeLanguage() {
+  let language = $("#languages").val();
 
-    let language = $("#languages").val();
-
-    if(language == 'c' || language == 'cpp')editor.session.setMode("ace/mode/c_cpp");
-    else if(language == 'php')editor.session.setMode("ace/mode/php");
-    else if(language == 'python')editor.session.setMode("ace/mode/python");
-    else if(language == 'node')editor.session.setMode("ace/mode/javascript");
+  if (language == "c" || language == "cpp")
+    editor.session.setMode("ace/mode/c_cpp");
+  else if (language == "php") editor.session.setMode("ace/mode/php");
+  else if (language == "python") editor.session.setMode("ace/mode/python");
+  else if (language == "node") editor.session.setMode("ace/mode/javascript");
 }
 
 function executeCode() {
+  console.log(editor.getSession().getValue());
+  $("#loading_spinner").addClass("loading");
+  $.ajax({
+    url: "https://stem.ee.cuhk.edu.hk/development/firmware/get_ESP_data.php?ID=7",
 
-    $.ajax({
+    method: "POST",
 
-        url: "/ide/app/compiler.php",
-
-        method: "POST",
-
-        data: {
-            language: $("#languages").val(),
-            code: editor.getSession().getValue()
-        },
-
-        success: function(response) {
-            $(".output").text(response)
-        }
-    })
+    data: {
+      code: editor.getSession().getValue(),
+    },
+    complete: () => {
+      $("#loading_spinner").removeClass("loading");
+    },
+    success: function (response) {
+      $(".output").text(response);
+    },
+  });
 }
